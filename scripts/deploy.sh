4b12d5d0543edc0ccc087a06cb0ce7d7e67f28f3
Tiago Boeing <contato@tiagoboeing.com>
2021-03-18 15:46:50 -0300

    Upload to CDN only when version was changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,9 +11,12 @@ echo "Prepare to invalidade cache on CloudFront"
 
 aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_DIST_ID} --paths "/platform/tecnologia-design-tokens/${GITHUB_REF##*/}/*"
 
-if [ ! -f $VERSION ]; then
-  echo "Uploading to $VERSION folder!"
+# upload only when version changed
+if [ ! -f $VERSION ] && [ ! -f $OLD_VERSION ]; then
+  if [ $VERSION != $OLD_VERSION ]; then
+    echo "Uploading to $VERSION folder!"
 
-  aws s3 sync ${BASE_PATH}dist ${BUCKET}${VERSION}
-  aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_DIST_ID} --paths "/platform/tecnologia-design-tokens/${VERSION}/*"
+    aws s3 sync ${BASE_PATH}dist ${BUCKET}${VERSION}
+    aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_DIST_ID} --paths "/platform/tecnologia-design-tokens/${VERSION}/*"
+  fi
 fi
