ccbb4b71f44f07ff068fa887ae6c17f3de18ef7d
Tiago Boeing <contato@tiagoboeing.com>
2021-03-18 15:26:20 -0300

    Change if condition

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ echo "Prepare to invalidade cache on CloudFront"
 
 aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_DIST_ID} --paths "/platform/tecnologia-design-tokens/${GITHUB_REF##*/}/*"
 
-if [ -f $VERSION ]; then
+if [ ! -f $VERSION ]; then
   echo "Uploading to $VERSION folder!"
 
   aws s3 sync ${BASE_PATH}dist ${BUCKET}${VERSION}
