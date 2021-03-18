e4cd1ea43dfe58386fa7dc4c473f208a3bbf668c
Tiago Boeing <contato@tiagoboeing.com>
2021-03-18 15:18:42 -0300

    Add upload to a version folder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,3 +10,10 @@ echo "Copy to S3 concluded"
 echo "Prepare to invalidade cache on CloudFront"
 
 aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_DIST_ID} --paths "/platform/tecnologia-design-tokens/${GITHUB_REF##*/}/*"
+
+if [ -f $VERSION ]; then
+  echo "Uploading to $VERSION folder!"
+
+  aws s3 sync ${BASE_PATH}dist ${BUCKET}${VERSION}
+  aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_DIST_ID} --paths "/platform/tecnologia-design-tokens/${VERSION}/*"
+fi
